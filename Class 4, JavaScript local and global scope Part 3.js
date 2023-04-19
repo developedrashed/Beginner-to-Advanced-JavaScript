f729949232cff2943d9196ccc41f85c1dd0fb047
developedrashed <developedrashed@gmail.com>
2023-04-19 08:45:03 +0600

    1 file modified, 1 added, 1 deleted

---
+++ Class 4, JavaScript local and global scope Part 3.js
@@ -0,0 +1,35 @@
+
+let name = 'Asraful Haque';
+
+function info(name, age, skill) {
+  let food = 'bereyani';
+  let play = 'PUBG';
+
+  console.log(play);
+  console.log(food); // Lookal Vareable
+}
+
+info();
+
+console.log(name);
+
+const naam = 'Asraful Haque';
+
+function info(name, age, skill) {
+
+  const naam;
+
+  return `My name is ${ naam }`;
+
+}
+
+console.log(info());
+
+const naam = 'Asraful Haque';
+
+function info(name, age, skill) {
+  
+  return `My name is ${naam}`;
+}
+
+console.log(info());
