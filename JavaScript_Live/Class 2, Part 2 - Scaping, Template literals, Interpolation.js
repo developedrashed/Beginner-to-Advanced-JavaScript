e72fdc45bb9faf9ff8aeda2cdc14a4f6509b72c9
developedrashed <developedrashed@gmail.com>
2023-04-20 08:28:56 +0600

    1 file modified, 1 added

---
+++ JavaScript_Live/Class 2, Part 2 - Scaping, Template literals, Interpolation.js
@@ -0,0 +1,9 @@
+console.log('I"m Haq');
+
+let name = prompt('Name');
+let age = prompt('Age');
+let Skill = prompt('Skill');
+
+console.log(`My name is ${name} & I am ${age} years old`);
+
+console.log(`I am Asraf ${5 * 9} and I like to eat kacchi`);
