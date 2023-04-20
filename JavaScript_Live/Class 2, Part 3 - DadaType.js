7ca445d02a11d7aa298bc65953158c30aeb6b51c
developedrashed <developedrashed@gmail.com>
2023-04-20 11:05:16 +0600

    1 file added

---
+++ JavaScript_Live/Class 2, Part 3 - DadaType.js
@@ -0,0 +1,11 @@
+// String
+// Number
+// Boolean
+// Array
+
+let name = 'Jillur Rahman'; // String
+let age = 100; // Number
+let call = '017'; // String
+let x = true; // boolean
+
+console.log(typeof call);
