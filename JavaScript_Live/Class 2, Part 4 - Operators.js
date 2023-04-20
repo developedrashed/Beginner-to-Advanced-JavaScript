56e52a8385bb59fbd99b2dee5752bd2143c9a2ba
developedrashed <developedrashed@gmail.com>
2023-04-20 12:11:46 +0600

    1 file modified, 1 added

---
+++ JavaScript_Live/Class 2, Part 4 - Operators.js
@@ -0,0 +1,47 @@
+let alo = 120;
+
+let lao = 400;
+
+let potol = 180;
+
+let tomtom = 160;
+
+let mot = alo + lao + potol + tomtom;
+
+console.log(`
+     
+      alor dam    = ${alo + 20} take
+      alor dam    = ${lao - 100} take
+      alor dam    = ${potol * 60} take
+                    ${tomtom / 80} taka
+-------------------------------------
+      mot khoroch    = ${mot} taka
+              Uttor =  ${mot} taka
+  
+
+`);
+
+
+
+Operators
+
+Arithmetic 
++, -, *, /, %, --, ++
+
+Assignment
+=       // let age = 30;
+
+Comparison
+== === <> <= >= != !===?
+
+Logical
+&& || !
+
+String
++ +=
+
+Bitwise
+& - ^ | << >> >>> <<<
+
+special (type operator)
+(?:), delete in instanceof type new void yield
