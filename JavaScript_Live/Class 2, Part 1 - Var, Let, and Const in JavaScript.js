bdd164ce75184344cf15e2988d67bb9be15628e8
developedrashed <developedrashed@gmail.com>
2023-04-20 08:12:14 +0600

    1 file modified, 1 added

---
+++ JavaScript_Live/Class 2, Part 1 - Var, Let, and Const in JavaScript.js
@@ -0,0 +1,38 @@
+var name;
+
+name = 'ram';
+
+name = 'lokhon';
+
+var name = 'kisno';
+
+console.log("I am " + name);
+
+let name;
+
+name = 'Loli';
+name = 'Mitu';
+name = 'Mina';
+
+console.log("I am " + name);
+
+const name = 'Asarf';
+
+console.log('I am ' + name);
+
+let _naam = 'kolim';
+let _boyoss = '100';
+
+console.log('I am ' + _naam + ' & I am ' + _boyoss + ' years old');
+
+let name = 'Asraf';
+let age = 20;
+let skill = 'MERN Stack';
+
+console.log(name);
+
+let name, food, skill;
+
+skill = 'Jasim';
+
+console.log(skill);
