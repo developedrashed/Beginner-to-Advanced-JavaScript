a15498999bc58227d8e8689110887e7a0c54de9d
developedrashed <developedrashed@gmail.com>
2023-04-25 18:17:44 +0600

    1 file modified, 1 added

---
+++ JavaScript_Live/Class 3, Part 4 - undefined, NULL, empty value in JavaScript.js
@@ -0,0 +1,15 @@
+let name;
+
+console.log(name); // undifind
+
+let name = '';
+
+console.log(name); // Spaice
+
+let name = null;
+
+console.log(name); // null
+
+let name = null;
+
+console.log(typeof name); // object
