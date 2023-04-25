7b21f3d6817c28febc9a15f284e281abd74bb14f
developedrashed <developedrashed@gmail.com>
2023-04-25 19:04:29 +0600

    1 file modified, 1 added

---
+++ Class 3, Part 5 - Ternary Operator & Null Coalescing Operator in JavaScript.js
@@ -0,0 +1,13 @@
+let age = 23;
+
+let beya = (age >= 18) ? 'beyar boyos done' : 'beyar boyos not done';
+
+console.log(beya);
+
+let age = null;
+
+console.log(age);
+
+let age = '';
+
+console.log(age ?? 0);
