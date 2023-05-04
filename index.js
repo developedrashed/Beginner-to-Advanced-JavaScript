bde814d34a9f482005102382b9531ca44339c3b3
developedrashed <developedrashed@gmail.com>
2023-05-04 17:14:51 +0600

    1 file modified

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// var fName = '10';
-// var address = 'Dinajput';
-// var phone = '017847839';
-// var lineBreak = '<br />';
 
-
-// document.write(fName);
-// document.write(lineBreak);
-// document.write(address);
-// document.write(lineBreak);
-// document.write(phone);
-
-
-// var firstNumber = 10;
-// var secondNumber = 20;
-// var resultAdd = firstNumber + secondNumber;
-
-// console.log(resultAdd);
 
  
